f77d31a8aeefc619283299e791012fa19530ca37
shashankgoud002 <101133749+shashankgoud001@users.noreply.github.com>
2024-01-16 13:02:47 +0530

    Coded for the functions: - linear probe - server_hash(modified) - request hash(modified)
    Added New classes:
    - ServerNode
    - RequestNode
    - Node

--- asgn1/consistent_hashing.py
+++ asgn1/consistent_hashing.py
@@ -1,26 +1,47 @@
+class ServerNode:
+    def __init__(self, i,j,ip,port):
+        self.server_id_i = i
+        self.server_id_j = j
+        self.server_ip = ip
+        self.server_port = port
+
+class RequestNode:
+    def __init__(self):
+        self.req = {}
+
+class Node:
+    def __init__(self):
+        self.server = None
+        self.requests = []
+
 # Map that holds virtual servers and requests
-map = {}
+circular_array = [None]*512
 
 # Consistent hashing parameters
 num_servers = 3
 num_slots = 512
 vir_servers = 9
 
+
 # Request mapping hash function
 def request_hash(i) -> int:
-    return (i**2 + 2*i + 17)
+    return (i**2 + 2*i + 17)%num_slots
 
 # Server mapping hash function
 def server_hash(i, j) -> int:
-    return (i**2 + j**2 + 2*i*j + 25)
+    return (i**2 + j**2 + 2*i*j + 25)%num_slots
 
 # Linear Probing for servers in case of collision
 def linear_probe(i, j):
     '''
         Linearly probes the next available
-        slot in the map for the server
+        slot in the map for the virtual server
     '''
-    pass
+    x = server_hash(i,j)%num_slots
+    while x in map:
+        x=(x+1)%num_slots
+    return x 
+    
 
 # Map request to server
 def map_request(i):
@@ -28,6 +49,7 @@ def map_request(i):
         Maps request to the nearest virtual server
         in the clockwise direction
     '''
+
     pass
 
 # Add Request
@@ -45,6 +67,7 @@ def add_virtual_server(i, j):
         maps the requests to this new virtual server
         accordingly
     '''
+    
     pass
 
 # Remove Request
